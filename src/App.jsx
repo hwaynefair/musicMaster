993e2190e64c0aa6c8570cc0c799730c8a351758
Wayne Fair <wayne@zurigroup.com>
2017-02-11 09:28:36 -0600

    test2

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ class App extends Component {
 
         return (
             <div className='App'>
-                <div className="App-title">Music Master Test</div>
+                <div className="App-title">Music Master Test2</div>
                 <FormGroup>
                     <InputGroup>
                         <FormControl
